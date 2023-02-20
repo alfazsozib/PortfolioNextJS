a6f789eab59af0a538d9c41bfefe62829eb24c25
alfazsozib <alfazsozib@gmail.com>
2023-02-20 23:49:58 +0600

    skill section done

--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { SiTailwindcss, SiHiveBlockchain ,SiReact, SiNodedotjs,SiCplusplus, SiNextdotjs, SiMongodb, SiHtml5, SiPython, SiWeb3Dotjs, SiEthereum } from "react-icons/si"
+import { SiTailwindcss,SiWordpress, SiHiveBlockchain ,SiReact, SiNodedotjs,SiCplusplus, SiNextdotjs, SiMongodb, SiHtml5, SiPython, SiWeb3Dotjs, SiEthereum } from "react-icons/si"
+import { RxDot } from "react-icons/rx"
 
 function Skill() {
             
@@ -8,22 +9,35 @@ function Skill() {
 
             <h1 className='text-[40px] pb-4 text-[#121111e8] font-saira font-bold'>SKILL</h1>
             <div className='experience-text--box flex justify-between mr-4'>
-                <h1 className='text-[22px] text-titleFont font-bold font-saira'>PROGRAMMING LANGUAGES</h1>
-                <p className='text-primaryText font-semibold text-sm'>20 Jul 2023</p>
+                <h1 className='text-[22px] text-titleFont font-bold font-saira'>PROGRAMMING LANGUAGE AND TOOLS</h1>
+                
             </div>
 
             <div className='text--box flex gap-8 py-12'>
-                <SiHtml5 size={55} className="text-gray-800"/>
-                <SiTailwindcss size={55} className="text-gray-800"/>
-                <SiReact size={55} className="text-gray-800"/>
-                <SiPython size={55} className="text-gray-800"/>
-                <SiCplusplus size={55} className="text-gray-800"/>
-                <SiNextdotjs size={55} className="text-gray-800"/>
-                <SiNodedotjs size={55} className="text-gray-800"/>
-                <SiMongodb size={55} className="text-gray-800"/>
-                <SiWeb3Dotjs size={55} className="text-gray-800"/>
-                <SiHiveBlockchain size={55} className="text-gray-800"/>
+                <SiHtml5 size={55} className="text-gray-600"/>
+                <SiTailwindcss size={55} className="text-gray-600"/>
+                <SiReact size={55} className="text-gray-600"/>
+                <SiPython size={55} className="text-gray-600"/>
+                <SiCplusplus size={55} className="text-gray-600"/>
+                <SiNextdotjs size={55} className="text-gray-600"/>
+                <SiNodedotjs size={55} className="text-gray-600"/>
+                <SiMongodb size={55} className="text-gray-600"/>
+                <SiWordpress size={55} className="text-gray-600"/>
+                <SiWeb3Dotjs size={55} className="text-gray-600"/>
+                <SiHiveBlockchain size={55} className="text-gray-600"/>
             </div>         
+            <div>
+                <h1 className='text-grayText text-xl font-semibold py-4'>WORKFLOW</h1>
+                <div className='pl-6'>
+                    <div>
+                        <p className='text-grayText flex items-center text-[17px]'> <RxDot /> Responsive Design For All Screen</p>
+                        <p className='text-grayText flex items-center text-[17px]'> <RxDot /> Single And Multiple Pages</p>
+                        <p className='text-grayText flex items-center text-[17px]'> <RxDot /> Web3 Development (BACKEND)</p>
+                        <p className='text-grayText flex items-center text-[17px]'> <RxDot /> Wallet Integration</p>
+                    </div>
+                    
+                </div>
+            </div>
         </div>
     )
 }
